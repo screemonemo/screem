23043213e02ecf7250981e06283e56063d8691fe
Tarun K Kumar <61784876+screemonemo@users.noreply.github.com>
2020-05-06 16:07:21 +0530

    Create scandoc.c

---
+++ scandoc.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+  FILE *fp=fopen("scandoc.txt","r");
+  int count=0;
+  char c;
+  while((c=fgetc(fp) && c!=EOF){
+    ++count;}
+  printf("The number of characters is : %d",count);
+  return 0;}
