e4e6ef0b7cce9f9d77161ec0a07cf464cb3c60de
Tarun K Kumar <61784876+screemonemo@users.noreply.github.com>
2020-05-06 16:51:21 +0530

    Update scandoc.c

--- scandoc.c
+++ scandoc.c
@@ -3,7 +3,7 @@ int main(){
   FILE *fp=fopen("mytext.txt","r");
   int count=0;
   char c;
-  while((c=fgetc(fp) && c!=EOF){
+  while((c=fgetc(fp)) && c!=EOF){
     ++count;}
   printf("The number of characters is : %d",count);
   return 0;}
